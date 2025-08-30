36b251637de84ec528ec3ae44c1878d1024acebe
tomech <109527747+tomech4@users.noreply.github.com>
2025-08-30 22:41:30 +0200

    fix init function in Person class

--- app/main.py
+++ app/main.py
@@ -4,8 +4,6 @@ class Person:
     def __init__(self, person: dict) -> None:
         self.name = person["name"]
         self.age = person["age"]
-        self.husband = person["husband"]
-        self.wife = person["wife"]
         people[person["name"]] = self
 
 
