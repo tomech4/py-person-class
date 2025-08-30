2bbbe055aebbc9aa8f887ed5b3f268fb94b5a93b
tomech <109527747+tomech4@users.noreply.github.com>
2025-08-30 22:39:11 +0200

    complete Person class definition

--- app/main.py
+++ app/main.py
@@ -1,6 +1,12 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, person: dict) -> None:
+        self.name = person["name"]
+        self.age = person["age"]
+        self.husband = person["husband"]
+        self.wife = person["wife"]
+        people[person["name"]] = self
 
 
 def create_person_list(people: list) -> list:
