985994b2da2dde79ae8437e5b1e0659252cf1a2c
tomech <109527747+tomech4@users.noreply.github.com>
2025-08-30 23:33:10 +0200

    complete create_person_list

--- app/main.py
+++ app/main.py
@@ -8,5 +8,16 @@ class Person:
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    list_of_persons = [
+        Person(person["name"], person["age"])
+        for person in people
+    ]
+
+    for person in people:
+        person_class = Person.people[person["name"]]
+        if person.get("wife"):
+            person_class.wife = Person.people[person["wife"]]
+        elif person.get("husband"):
+            person_class.husband = Person.people[person["husband"]]
+
+    return list_of_persons
